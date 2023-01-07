ca1aef6b9bb24c50e33a97fbe9a427466fbef061
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-07 01:33:32 +0000

    Fix linter errors

--- app.rb
+++ app.rb
@@ -101,7 +101,7 @@ class App
       print 'ID of person: '
       id_selected = gets.chomp.to_i
       Rental.all.each do |rental|
-        if rental.person.id = id_selected
+        if rental.person.id == id_selected
           puts %(#{rental.date}, Book "#{rental.book.title}" by #{rental.book.author}, rented by #{rental.person.name})
         else
           puts 'There\'s no rental with that ID'
