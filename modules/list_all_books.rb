2d42e4dc528ae97635647c5a06cb555da4961070
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-09 19:46:58 +0000

    Add modules directory

---
+++ modules/list_all_books.rb
@@ -0,0 +1,8 @@
+module ListAllBooks
+  def list_all_books
+    puts 'There\'s no books added yet!' if Book.all.empty?
+    Book.all.each do |book|
+      puts %(Title: #{book.title}, Author: #{book.author})
+    end
+  end
+end
