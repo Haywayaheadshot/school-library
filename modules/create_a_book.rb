dc4864f9bc774acc65e2cf9bedd5cb58c3dbfdf4
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-10 23:24:18 +0000

    Create books_arr varible and push new books in array before storing

--- modules/create_a_book.rb
+++ modules/create_a_book.rb
@@ -1,15 +1,17 @@
-require './storage'
+require './modules/storage'
 require 'pry'
 
 module CreateABook
+  @@books_arr = []
+
   def create_a_book
     print 'Title: '
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
     # binding.pry
-    # Book.new(title, author)
-    Storage.save_date('books', Book.new(title, author).to_json)
+    @@books_arr.push(Book.new(title, author).to_json)
+    Storage.save_date('books', @@books_arr)
     puts 'Book succesfully created!'
   end
 end
