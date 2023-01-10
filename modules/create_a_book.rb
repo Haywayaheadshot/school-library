013374c6631e3d928ab74202e0c55e760d45892e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-10 20:53:35 +0000

    Require storage to store data and pry to check error foundation

--- modules/create_a_book.rb
+++ modules/create_a_book.rb
@@ -1,10 +1,15 @@
+require './storage'
+require 'pry'
+
 module CreateABook
   def create_a_book
     print 'Title: '
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
-    Book.new(title, author)
+    # binding.pry
+    # Book.new(title, author)
+    Storage.save_date('books', Book.new(title, author).to_json)
     puts 'Book succesfully created!'
   end
 end
