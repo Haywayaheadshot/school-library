c307889cd32ac37059d35ad378fc573af55a31de
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 23:14:42 +0000

    Save people and rentals array

--- modules/exit_app.rb
+++ modules/exit_app.rb
@@ -4,7 +4,7 @@ module ExitApp
   def exit_app
     puts 'Thanks for using my library!'
     Storage.save_data('books', @books_arr)
-    Storage.save_data('student', @student_arr)
-    Storage.save_data('teacher', @teacher_arr)
+    Storage.save_data('people', @people_arr)
+    Storage.save_data('rental', @rentals_arr)
   end
 end
