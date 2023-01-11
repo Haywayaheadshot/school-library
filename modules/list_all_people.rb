fb34a1e40a111a85b35b61e6cb002eb51c89c4e4
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 23:13:55 +0000

    Refactor code to to check for a key and differentiate between teachers and students and list appropriately

--- modules/list_all_people.rb
+++ modules/list_all_people.rb
@@ -1,29 +1,26 @@
 module ListAllPeople
   def list_all_people
-    stored_student_data = Storage.load_data('student')
-    stored_teacher_data = Storage.load_data('teacher')
-    if @student_arr.empty? && @teacher_arr.empty? && stored_student_data.empty? && stored_teacher_data.empty?
+    stored_people_data = Storage.load_data('people')
+    if @people_arr.empty? && stored_people_data.empty?
       puts 'There\'s no people added yet'
-    end
-    if @student_arr.length >= 1
-      @student_arr.each do |student|
-        puts %([Student] Name: #{student['name']}, Age: #{student['age']})
+    elsif @people_arr.length >= 1
+      @people_arr.each do |person|
+        if person.key?('specialization')
+          puts %([Teacher] Name: #{person['name']}, ID: #{person['id']},
+          Age: #{person['age']}, Specialization: #{person['specialization']})
+        else
+          puts %([Student] Name: #{person['name']}, ID: #{person['id']} Age: #{person['age']})
+        end
       end
-    end
-    if @teacher_arr.length >= 1
-      @teacher_arr.each do |teacher|
-        puts %([Teacher] Name: #{teacher['name']}, Age: #{teacher['age']})
+    else
+      stored_people_data.each do |person|
+        if person.key?('specialization')
+          puts %([Teacher] Name: #{person['name']}, ID: #{person['id']},
+          Age: #{person['age']}, Specialization: #{person['specialization']})
+        else
+          puts %([Student] Name: #{person['name']}, ID: #{person['id']} Age: #{person['age']})
+        end
       end
-    end
-    if stored_student_data.length >= 1
-      stored_student_data.each do |student|
-        puts %([Student] Name: #{student['name']}, Age: #{student['age']})
-      end
-    end
-    return unless stored_teacher_data.length >= 1
-
-    stored_teacher_data.each do |teacher|
-      puts %([Teacher] Name: #{teacher['name']}, Age: #{teacher['age']})
     end
   end
 end
