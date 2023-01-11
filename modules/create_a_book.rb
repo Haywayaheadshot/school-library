3dd48b3dc5bf47faa135fdf31253114c51c4a1e8
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 18:41:12 +0000

    Push new book instances into books array in app class

--- modules/create_a_book.rb
+++ modules/create_a_book.rb
@@ -1,17 +1,11 @@
-require './modules/storage'
-require 'pry'
-
 module CreateABook
-  @@books_arr = []
-
   def create_a_book
     print 'Title: '
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
-    # binding.pry
-    @@books_arr.push(Book.new(title, author).to_json)
-    Storage.save_date('books', @@books_arr)
+    book = Book.new(title, author)
+    @books_arr.push({ 'title' => book.title, 'author' => book.author })
     puts 'Book succesfully created!'
   end
 end
