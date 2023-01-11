e748a1e1a0e69f65acd46199cd62bcf731dd5265
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 19:19:23 +0000

    Display people if user checks before closing exiting app

--- modules/list_all_people.rb
+++ modules/list_all_people.rb
@@ -2,8 +2,19 @@ module ListAllPeople
   def list_all_people
     stored_student_data = Storage.load_data('student')
     stored_teacher_data = Storage.load_data('teacher')
-
-    puts 'There\'s no people added yet' if stored_student_data.empty? && stored_teacher_data.empty?
+    if @student_arr.empty? && @teacher_arr.empty? && stored_student_data.empty? && stored_teacher_data.empty?
+      puts 'There\'s no people added yet'
+    end
+    if @student_arr.length >= 1
+      @student_arr.each do |student|
+        puts %([Student] Name: #{student['name']}, Age: #{student['age']})
+      end
+    end
+    if @teacher_arr.length >= 1
+      @teacher_arr.each do |teacher|
+        puts %([Teacher] Name: #{teacher['name']}, Age: #{teacher['age']})
+      end
+    end
     if stored_student_data.length >= 1
       stored_student_data.each do |student|
         puts %([Student] Name: #{student['name']}, Age: #{student['age']})
