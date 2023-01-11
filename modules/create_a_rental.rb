d8cb040581853dd240d5060145f413fa1d0165cf
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 23:16:08 +0000

    Properly collect and push rental details to rentals array

--- modules/create_a_rental.rb
+++ modules/create_a_rental.rb
@@ -1,23 +1,26 @@
+require 'pry'
+
 module CreateARental
   def create_a_rental
-    if Book.all.empty? || Person.all.empty?
+    if @people_arr.empty? || @books_arr.empty?
       puts 'There\'s no persons or books added yet!'
     else
       puts 'Select a book from the following list by number'
-      Book.all.each_with_index do |book, index|
-        puts %(#{index}\) Title: "#{book.title}", Author: #{book.author})
+      @books_arr.each_with_index do |book, index|
+        puts %(#{index}\) Title: "#{book['title']}", Author: #{book['author']})
       end
       chosen_book = gets.chomp.to_i
-      Student.all.each_with_index do |student, index|
-        puts %(#{index}\) [Student] Name: #{student.name}, ID: #{student.id}, Age: #{student.age})
-      end
-      Teacher.all.each_with_index do |teacher, index|
-        puts %(#{index}\) [Teacher] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age})
+      @people_arr.each_with_index do |person, index|
+        puts %(#{index}\) [Student] Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']})
       end
       selected_person = gets.chomp.to_i
       puts 'Date: '
       date = gets.chomp
-      Rental.new(date, Book.all[chosen_book], Person.all[selected_person])
+      rental = Rental.new(date,
+                          { 'title' => @books_arr[chosen_book]['title'], 'author' => @books_arr[chosen_book]['author'] }, { 'person' => @people_arr[selected_person]['id'], 'name' => @people_arr[selected_person]['name'] })
+      @rentals_arr.push({ 'id' => rental.id, 'date' => date, 'person' => rental.person['person'],
+                          'title' => rental.book['title'], 'author' => rental.book['author'], 'name' => rental.person['name'] })
+      # binding.pry
       puts 'Rental created succesfully'
     end
   end
