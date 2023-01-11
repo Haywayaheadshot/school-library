7ce70d3a24dc10f8c6f6e5a9c9dbc16e4a97ec6c
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-11 23:12:12 +0000

    Refactor code to demand for user id and check for rental availability

--- modules/list_all_rentals.rb
+++ modules/list_all_rentals.rb
@@ -1,13 +1,29 @@
 module ListAllRentals
   def list_all_rentals
-    if Person.all.empty? || Book.all.empty?
-      puts 'Please add a person and a book first!'
+    stored_rental_data = Storage.load_data('rental')
+    if stored_rental_data.empty?
+      if @people_arr.empty? || @books_arr.empty?
+        puts 'Please add a person and a book first!'
+      else
+        print 'ID of person: '
+        id_selected = gets.chomp.to_i
+
+        if @rentals_arr.length >= 1
+          @rentals_arr.each do |rental|
+            if rental['person'] == id_selected
+              puts %(#{rental['date']}, Book "#{rental['title']}" by #{rental['author']}, rented by #{rental['name']})
+            else
+              puts 'There\'s no rental with that ID'
+            end
+          end
+        end
+      end
     else
       print 'ID of person: '
       id_selected = gets.chomp.to_i
-      Rental.all.each do |rental|
-        if rental.person.id == id_selected
-          puts %(#{rental.date}, Book "#{rental.book.title}" by #{rental.book.author}, rented by #{rental.person.name})
+      stored_rental_data.each do |rental|
+        if rental['person'] == id_selected
+          puts %(#{rental['date']}, Book "#{rental['title']}" by #{rental['author']}, rented by #{rental['name']})
         else
           puts 'There\'s no rental with that ID'
         end
