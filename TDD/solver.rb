577be8fe089aaf5750c778cca1a793a3b71e59cf
Oklukeok <lucasabermudez@gmail.com>
2023-01-13 09:59:49 -0600

    bug fix

--- TDD/solver.rb
+++ TDD/solver.rb
@@ -1,20 +1,20 @@
 class Solver
-  def factorial(_num)
-    return 'The number its negative' if n.negative?
-    return 1 unless n.positive?
+  def factorial(num)
+    return 'The number its negative' if num.negative?
+    return 1 unless num.positive?
 
-    n * factorial(n - 1)
+    num * factorial(num - 1)
   end
 
   def reverse(word)
     word.reverse
   end
 
-  def fizzbuzz(_num)
-    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
-    return 'fizz' if (n % 3).zero?
-    return 'buzz' if (n % 5).zero?
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
 
-    n.to_s
+    num.to_s
   end
 end
